c24372d2a6be6d2de96103fb6cb8e19f908515ce
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2026-07-05 17:34:17 +0530

    complete Week 5 MongoDB database integration

--- backend/models/description.js
+++ backend/models/description.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const descriptionSchema = new mongoose.Schema(
   {
-    productName: {
+    productName:{
       type: String,
       required: true,
     },
