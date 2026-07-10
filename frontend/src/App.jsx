d90159859d586345273949e763c2b60c7f4e81fa
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2026-07-10 17:37:11 +0530

    Frontend design change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Generator from "./pages/Generator";
